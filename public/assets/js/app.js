ad3e64a75a2f4e9b351da62917dc22f72d3c9000
Joska Potin <jpotin@gmail.com>
2021-09-28 23:08:11 +0200

    menu and recipe components

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,52 @@
+import recipes from "./data/recipes.js"
 import dropdownModule from "./modules/dropdown.module.js"
+import uiDropdownMenu from "./components/dropdown-menu.component.js"
+import uiRecipe from "./components/recipe.component.js"
 
 dropdownModule()
+
+// build default ingredients menu
+const getAllIngredients = recipes => {
+  const allIngredientsDup = recipes.flatMap(recipe => {
+    return recipe.ingredients.map(ingredients => {
+      return ingredients.ingredient.toLowerCase()
+    })
+  })
+
+  return [...new Set(allIngredientsDup.sort())]
+}
+
+const uiIngredientsMenu = document.querySelector(".dropdown-ingredients")
+uiIngredientsMenu.appendChild(uiDropdownMenu(getAllIngredients(recipes)))
+
+// build default appliance menu
+const getAllappliances = recipes => {
+  const allappliancesDup = recipes.map(recipe => {
+    return recipe.appliance.toLowerCase()
+  })
+
+  return [...new Set(allappliancesDup.sort())]
+}
+
+const uiAppliancesMenu = document.querySelector(".dropdown-appliance")
+uiAppliancesMenu.appendChild(uiDropdownMenu(getAllappliances(recipes)))
+
+// build default ustensils menu
+const getAllUstensils = recipes => {
+  const allUstensilsDup = recipes.flatMap(recipe => {
+    return recipe.ustensils.map(ustensil => {
+      return ustensil.toLowerCase()
+    })
+  })
+
+  return [...new Set(allUstensilsDup.sort())]
+}
+
+const uiUstensilsMenu = document.querySelector(".dropdown-ustensils")
+uiUstensilsMenu.appendChild(uiDropdownMenu(getAllUstensils(recipes)))
+
+// build default recipe
+const uiRecipesList = document.getElementById("recipes-list")
+recipes.forEach(recipe => {
+  uiRecipesList.appendChild(uiRecipe(recipe))
+})
---
+++ public/assets/js/components/recipe.component.js
@@ -0,0 +1,54 @@
+const uiRecipe = ({ name, ingredients, time, description }) => {
+  const uiRecipe = document.createElement("article")
+  uiRecipe.classList.add("card")
+
+  const uiHeader = document.createElement("header")
+  uiHeader.classList.add("card__header")
+
+  const uiHeading = document.createElement("h2")
+  uiHeading.textContent = name
+  uiHeading.classList.add("card__heading")
+
+  const uiTime = document.createElement("span")
+  uiTime.textContent = `${time} min`
+
+  const uiContent = document.createElement("div")
+  uiContent.classList.add("card__content")
+
+  const uiIngredientsList = document.createElement("ul")
+  uiIngredientsList.classList.add("card__list")
+
+  ingredients.forEach(ingredient => {
+    const uiIngredientItem = document.createElement("li")
+    uiIngredientItem.classList.add("card__list__item")
+
+    const uiIngredient = document.createElement("span")
+    uiIngredient.textContent = ingredient.ingredient
+
+    uiIngredientItem.appendChild(uiIngredient)
+
+    if (ingredient.quantity) {
+      const uiQuantity = document.createTextNode(`: ${ingredient.quantity}`)
+      uiIngredientItem.appendChild(uiQuantity)
+    }
+
+    if (ingredient.unit) {
+      const uiUnit = document.createTextNode(` ${ingredient.unit}`)
+      uiIngredientItem.appendChild(uiUnit)
+    }
+
+    uiIngredientsList.appendChild(uiIngredientItem)
+  })
+
+  const uiDescription = document.createElement("div")
+  uiDescription.textContent = description
+  uiDescription.classList.add("card__description")
+
+  uiHeader.append(uiHeading, uiTime)
+  uiContent.append(uiIngredientsList, uiDescription)
+  uiRecipe.append(uiHeader, uiContent)
+
+  return uiRecipe
+}
+
+export default uiRecipe

7b8c42b573c1d3b61c6904f2f09fc18d8ec6b32c
joskapotin <joskapotin@users.noreply.github.com>
2021-10-17 23:28:20 +0000

    only 2 loop but no local storage d6bea8a13c3e6c6cdd5e7abd0a3a3b101c267991

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,131 +14,197 @@ const uiUstensilsMenu = document.querySelector(".dropdown-ustensils")
 // VARIABLES
 const filters = { main: [], ingredients: [], appliances: [], ustensils: [] }
 
-// get filters elements to populate secondary search menu
-const getIngredients = recipes => {
-  const ingredientsDup = recipes.flatMap(recipe => {
-    return recipe.ingredients.map(ingredients => {
-      return ingredients.ingredient.toLowerCase()
-    })
-  })
+// SECTION RENDER
 
-  return [...new Set(ingredientsDup.sort())]
-}
-
-const getAppliances = recipes => {
-  const appliancesDup = recipes.map(recipe => {
-    return recipe.appliance.toLowerCase()
-  })
-
-  return [...new Set(appliancesDup.sort())]
-}
-
-const getUstensils = recipes => {
-  const ustensilsDup = recipes.flatMap(recipe => {
-    return recipe.ustensils.map(ustensil => {
-      return ustensil.toLowerCase()
-    })
-  })
-
-  return [...new Set(ustensilsDup.sort())]
-}
-
-// populate secondary search menus
-const populateSecondaryMenu = ({ uiMenu, filterTerms, filterCat }) => {
+// render secondary search menus
+const renderSecondaryMenu = ({ uiMenu, filterTerms, filterCat }) => {
   uiMenu.querySelector(".dropdown-menu")?.remove()
   uiMenu.appendChild(uiDropdownMenu({ filterTerms, filterCat }))
 }
 
-// populate filters list
-const populateFiltersList = () => {
+// render filters list
+const renderFiltersList = () => {
   document.querySelector(".filters-list")?.remove()
   uiNavSecondary.parentNode.insertBefore(uiFiltersList(filters), uiNavSecondary)
 }
 
-// populate recipes list
-const populateRecipesList = recipes => {
+// render recipes list
+const renderRecipesList = matchRecipes => {
   const uiRecipesList = document.getElementById("recipes-list")
   uiRecipesList.innerHTML = ""
-  recipes.forEach(recipe => {
+  matchRecipes.forEach(recipe => {
     uiRecipesList.appendChild(uiRecipe(recipe))
   })
 }
 
-// RENDER HTML
-const render = (matchRecipes = recipes) => {
-  matchRecipes = matchRecipes.sort((a, b) => a.name.localeCompare(b.name))
-  populateSecondaryMenu({ uiMenu: uiIngredientsMenu, filterTerms: getIngredients(matchRecipes), filterCat: "ingredients" })
-  populateSecondaryMenu({ uiMenu: uiAppliancesMenu, filterTerms: getAppliances(matchRecipes), filterCat: "appliances" })
-  populateSecondaryMenu({ uiMenu: uiUstensilsMenu, filterTerms: getUstensils(matchRecipes), filterCat: "ustensils" })
-  populateRecipesList(matchRecipes)
+// render HTML
+const render = ({ matchRecipes, ingredients, appliances, ustensils }) => {
+  renderFiltersList()
+  renderSecondaryMenu({ uiMenu: uiIngredientsMenu, filterTerms: ingredients, filterCat: "ingredients" })
+  renderSecondaryMenu({ uiMenu: uiAppliancesMenu, filterTerms: appliances, filterCat: "appliances" })
+  renderSecondaryMenu({ uiMenu: uiUstensilsMenu, filterTerms: ustensils, filterCat: "ustensils" })
+  renderRecipesList(matchRecipes)
 }
 
-// TESTS
-const isName = ({ filterTerm, recipe }) => recipe.name.toLowerCase().includes(filterTerm.toLowerCase())
-
-const isIngredient = ({ filterTerm, recipe }) => recipe.ingredients.some(ingredients => ingredients.ingredient.toLowerCase().includes(filterTerm.toLowerCase()))
-
-const isAppliance = ({ filterTerm, recipe }) => recipe.appliance.toLowerCase().includes(filterTerm.toLowerCase())
-
-const isUstensil = ({ filterTerm, recipe }) => recipe.ustensils.some(ustensil => ustensil.toLowerCase().includes(filterTerm.toLowerCase()))
-
-const isDescription = ({ filterTerm, recipe }) => recipe.description.toLowerCase().includes(filterTerm.toLowerCase())
-
-// SEARCH
+// SECTION TESTS
 
 /**
- * Search recipes
- * @param {string} filterCat
- * @returns {string[]} ids of recipe to remove from the list
+ * Test if a term is include in the name of the recipe
+ * @param {{filterTerm:string,recipe:object}}
+ * @returns {boolean} true or false
  */
-const searchRecipes = filterCat => {
-  const filterTerms = Object.values(filters[filterCat])
+const isName = ({ filterTerm, recipe }) => recipe.name.toLowerCase().includes(filterTerm.toLowerCase())
 
-  const discardedRecipesIds = filterTerms.reduce((ids, filterTerm) => {
-    if (localStorage.getItem(`${filterCat}-${filterTerm}`)) {
-      ids = [...ids, ...JSON.parse(localStorage.getItem(`${filterCat}-${filterTerm}`))]
-    } else {
-      const idsFounded = recipes.reduce((idsByfilterCat, recipe) => {
-        if (filterCat === "main" && !isName({ filterTerm, recipe }) && !isIngredient({ filterTerm, recipe }) && !isDescription({ filterTerm, recipe })) idsByfilterCat.push(recipe.id)
-        if (filterCat === "ingredients" && !isIngredient({ filterTerm, recipe })) idsByfilterCat.push(recipe.id)
-        if (filterCat === "appliances" && !isAppliance({ filterTerm, recipe })) idsByfilterCat.push(recipe.id)
-        if (filterCat === "ustensils" && !isUstensil({ filterTerm, recipe })) idsByfilterCat.push(recipe.id)
-        return idsByfilterCat
-      }, [])
+/**
+ * Test if a term is include in the ingredients of the recipe
+ * @param {{filterTerm:string,recipe:object}}
+ * @returns {boolean} true or false
+ */
+const isIngredient = ({ filterTerm, recipe }) => recipe.ingredients.some(ingredients => ingredients.ingredient.toLowerCase().includes(filterTerm.toLowerCase()))
 
-      ids = [...new Set([...ids, ...idsFounded])]
-      localStorage.setItem(`${filterCat}-${filterTerm}`, JSON.stringify(ids))
-    }
-    return ids
-  }, [])
+/**
+ * Test if a term is include in the appliances of the recipe
+ * @param {{filterTerm:string,recipe:object}}
+ * @returns {boolean} true or false
+ */
+const isAppliance = ({ filterTerm, recipe }) => recipe.appliance.toLowerCase().includes(filterTerm.toLowerCase())
 
-  // console.log("discardedRecipesIds", discardedRecipesIds)
-  // console.log("New set discardedRecipesIds", [...new Set(discardedRecipesIds)])
+/**
+ * Test if a term is include in the ustensils of the recipe
+ * @param {{filterTerm:string,recipe:object}}
+ * @returns {boolean} true or false
+ */
+const isUstensil = ({ filterTerm, recipe }) => recipe.ustensils.some(ustensil => ustensil.toLowerCase().includes(filterTerm.toLowerCase()))
 
-  return [...new Set(discardedRecipesIds)]
+/**
+ * Test if a term is include in the description of the recipe
+ * @param {{filterTerm:string,recipe:object}}
+ * @returns {boolean} true or false
+ */
+const isDescription = ({ filterTerm, recipe }) => recipe.description.toLowerCase().includes(filterTerm.toLowerCase())
+
+// SECTION SEARCH
+
+const getIngredients = recipe => {
+  return recipe.ingredients.map(ingredients => {
+    return ingredients.ingredient.toLowerCase()
+  })
 }
 
-// UPDATE RECIPES LIST
-const updateRecipesList = () => {
-  // If theres is no search or filter render default view
-  if (filters.main.length === 0 && filters.ingredients.length === 0 && filters.appliances.length === 0 && filters.ustensils.length === 0) return render()
-
-  // create an array of match recipes
-  const discardedRecipesIds = Object.keys(filters)
-    .filter(filterCat => filters[filterCat].length > 0)
-    .flatMap(filterCat => searchRecipes(filterCat))
-
-  /**
-   * Create list of match recipes
-   */
-  const matchRecipes = recipes.reduce((recipes, recipe) => {
-    if (!discardedRecipesIds.includes(recipe.id)) recipes.push(recipe)
-    return recipes
-  }, [])
-
-  // render view
-  render(matchRecipes)
+const getAppliances = recipe => {
+  return [recipe.appliance.toLowerCase()]
 }
+
+const getUstensils = recipe => {
+  return recipe.ustensils.map(ustensil => {
+    return ustensil.toLowerCase()
+  })
+}
+
+/**
+ * loop the recipes to get the items and return them without dups
+ * use only when there is no filters
+ * @param {recipes[]} recipes
+ * @returns {{ingredients:string[],appliances:string[],ustensils:string[]}}
+ */
+const getSecondaryMenuItems = recipes => {
+  let ingredientsDup = []
+  let appliancesDup = []
+  let ustensilsDup = []
+
+  recipes.forEach(recipe => {
+    ingredientsDup = [...ingredientsDup, ...getIngredients(recipe)]
+    appliancesDup = [...appliancesDup, ...getAppliances(recipe)]
+    ustensilsDup = [...ustensilsDup, ...getUstensils(recipe)]
+  })
+
+  console.log(appliancesDup)
+
+  const secondaryMenuItems = { ingredients: [...new Set(ingredientsDup.sort())], appliances: [...new Set(appliancesDup.sort())], ustensils: [...new Set(ustensilsDup.sort())] }
+
+  return secondaryMenuItems
+}
+
+/**
+ * Filter recipes
+ * @returns {recipes[]} recipes that match the filters
+ */
+const createMatchRecipes = () => {
+  // if no filters return all recipes
+  if (filters.main.length === 0 && filters.ingredients.length === 0 && filters.appliances.length === 0 && filters.ustensils.length === 0) {
+    const matchRecipes = recipes.sort((a, b) => a.name.localeCompare(b.name))
+    const { ingredients, appliances, ustensils } = getSecondaryMenuItems(matchRecipes)
+    render({ matchRecipes, ingredients, appliances, ustensils })
+    return
+  }
+
+  // create an array with only the active filters
+  const activeFilterCats = Object.keys(filters).filter(filterCat => filters[filterCat].length > 0)
+
+  // // create results storage object
+  // const resultStorage = {
+  //   filters: {
+  //     main: [],
+  //     ingredients: [],
+  //     appliances: [],
+  //     ustensils: [],
+  //   },
+  //   recipeId: {},
+  // }
+
+  // // store result
+  // const storeResult = ({ filterCat, filterTerm, recipe, ingredients = [], appliances = [], ustensils = [] }) => {
+  //   // store recipes by filter category and by term
+  //   if (resultStorage.filters[filterCat][filterTerm]) resultStorage.filters[filterCat][filterTerm].push(recipe)
+  //   else resultStorage.filters[filterCat][filterTerm] = [recipe]
+
+  //   // store items by recipe id and type
+  //   if (!resultStorage.recipeId[recipe.id]) {
+  //     resultStorage.recipeId[recipe.id] = { ingredients: [...ingredients], appliances: [...appliances], ustensils: [...ustensils] }
+  //   }
+  // }
+
+  // console.log(resultStorage)
+
+  const isValidRecipe = recipe => {
+    let isValid = true
+
+    activeFilterCats.forEach(filterCat => {
+      filters[filterCat].forEach(filterTerm => {
+        if (filterCat === "main" && !isName({ filterTerm, recipe }) && !isIngredient({ filterTerm, recipe }) && !isDescription({ filterTerm, recipe })) {
+          isValid = false
+          // storeResult({ filterCat, filterTerm, recipe })
+        }
+        if (filterCat === "ingredients" && !isIngredient({ filterTerm, recipe })) {
+          isValid = false
+          // storeResult({ filterCat, filterTerm, recipe, ingredients: getIngredients(recipe) })
+        }
+        if (filterCat === "appliances" && !isAppliance({ filterTerm, recipe })) {
+          isValid = false
+          // storeResult({ filterCat, filterTerm, recipe, appliances: getAppliances(recipe) })
+        }
+        if (filterCat === "ustensils" && !isUstensil({ filterTerm, recipe })) {
+          isValid = false
+          // storeResult({ filterCat, filterTerm, recipe, ustensils: getUstensils(recipe) })
+        }
+      })
+    })
+
+    return isValid
+  }
+
+  const matchRecipes = recipes
+    .filter(recipe => {
+      return isValidRecipe(recipe)
+    })
+    .sort((a, b) => a.name.localeCompare(b.name))
+
+  const { ingredients, appliances, ustensils } = getSecondaryMenuItems(matchRecipes)
+
+  render({ matchRecipes, ingredients, appliances, ustensils })
+}
+
+// SECTION ACTION
 
 // handle primary search
 uiSearchPrimary.addEventListener("input", e => {
@@ -146,7 +212,8 @@ uiSearchPrimary.addEventListener("input", e => {
   if (e.target.value.length >= 3) {
     filters.main[0] = e.target.value
   }
-  updateRecipesList()
+
+  createMatchRecipes()
 })
 
 // handle filterTerms search
@@ -167,10 +234,9 @@ document.addEventListener("click", e => {
     filters[type].push(filterTerm)
   }
 
-  populateFiltersList()
-  updateRecipesList()
+  createMatchRecipes()
 })
 
-// INIT
+// SECTION INIT
 dropdownModule()
-render()
+createMatchRecipes()

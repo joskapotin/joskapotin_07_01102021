246ef2c0438d54532125df781bd2f9857a64b2b6
joskapotin <joskapotin@users.noreply.github.com>
2021-11-13 16:52:49 +0000

    fix filename 39856cca191c82a1248e0c1410838d53c720963d

---
+++ assets/js/utils/utils.js
@@ -0,0 +1,11 @@
+/**
+ * Sanitize a string
+ * @param {string} string - raw string
+ * @return {string} sanitized string
+ */
+const sanitize = string => {
+  const regex = new RegExp(/[".]/g)
+  return string.toLowerCase().replace(regex, "")
+}
+
+export { sanitize }

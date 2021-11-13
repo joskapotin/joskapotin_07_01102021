387eb415db24f66374ae2fff5bd77b28d9e0fc81
joskapotin <joskapotin@users.noreply.github.com>
2021-11-13 16:51:55 +0000

    try to fix filename 3b3ff6a334f5f6e1dbc22fbf4834751d73362c96

--- assets/js/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  uiSearchPrimary: document.querySelector(".form-control-primary"),
-  uiNavSecondary: document.querySelector(".nav-secondary"),
-  uiSecondaryMenus: document.querySelectorAll("[data-dropdown]"),
-  uiIngredientsMenu: document.querySelector(".dropdown-ingredients"),
-  uiAppliancesMenu: document.querySelector(".dropdown-appliances"),
-  uiUstensilsMenu: document.querySelector(".dropdown-ustensils"),
-  uiRecipesList: document.getElementById("recipes-list"),
-}

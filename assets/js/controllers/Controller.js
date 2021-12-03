161daf71d108e5877bd712b007a2312ca00f0c91
joskapotin <joskapotin@users.noreply.github.com>
2021-12-03 17:32:07 +0000

    move function in recipe obj 4510b481ba1c049e03feece79f00a8b762cd4180

--- assets/js/controllers/Controller.js
+++ assets/js/controllers/Controller.js
@@ -1,10 +1,10 @@
-import AllRecipes from "../data/recipes.js"
+import allRecipes from "../data/recipes.js"
 import Recipe from "../models/Recipe.js"
 import View from "../views/View.js"
 
 /**
  * Main function that loop through every recipes and return the data to render
- * Time complexity O(n) * O(1) * O(1) = O(n)
+ * Time complexity O(n)
  *
  * @param {object} filter
  * @return {{matchRecipes:object[],ingredients:object,appliances:object,ustensils:object}}
@@ -15,8 +15,8 @@ const getMatchRecipes = filter => {
   let appliancesObj = {}
   let ustensilsObj = {}
 
-  AllRecipes.forEach(element => {
-    const recipe = new Recipe(element)
+  for (let i = 0; i < allRecipes.length; i++) {
+    const recipe = new Recipe(allRecipes[i])
 
     if (recipe.isMatchRecipe(filter)) {
       matchRecipes.push(recipe)
@@ -24,7 +24,7 @@ const getMatchRecipes = filter => {
       appliancesObj = { ...appliancesObj, ...recipe.appliancesList }
       ustensilsObj = { ...ustensilsObj, ...recipe.ustensilsList }
     }
-  })
+  }
 
   const ingredients = Object.keys(ingredientsObj).sort()
   const appliances = Object.keys(appliancesObj).sort()

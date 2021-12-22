4d2e41283ccf996bcb38f938c01a280db7571846
joskapotin <joskapotin@users.noreply.github.com>
2021-12-22 11:15:31 +0000

    add err msg when no recipe found 850a969d0324c5eb17d3a2932bced925c717a109

--- assets/js/models/Recipe.js
+++ assets/js/models/Recipe.js
@@ -26,4 +26,56 @@ export default class {
     const entries = this.ustensils.map(ustensil => [sanitize(ustensil), true])
     return Object.fromEntries(entries)
   }
+
+  isName(filterTerm) {
+    return this.name.toLowerCase().includes(filterTerm.toLowerCase())
+  }
+
+  isIngredient(filterTerm) {
+    return this.ingredients.some(ingredients => ingredients.ingredient.toLowerCase().includes(filterTerm.toLowerCase()))
+  }
+
+  isAppliance(filterTerm) {
+    return this.appliance.toLowerCase().includes(filterTerm.toLowerCase())
+  }
+
+  isUstensil(filterTerm) {
+    return this.ustensils.some(ustensil => ustensil.toLowerCase().includes(filterTerm.toLowerCase()))
+  }
+
+  isDescription(filterTerm) {
+    return this.description.toLowerCase().includes(filterTerm.toLowerCase())
+  }
+
+  isMatchMain(filterTerms) {
+    return filterTerms.every(filterTerm => this.isName(filterTerm) || this.isIngredient(filterTerm) || this.isDescription(filterTerm))
+  }
+
+  isMatchIngredients(filterTerms) {
+    return filterTerms.every(filterTerm => this.isIngredient(filterTerm))
+  }
+
+  isMatchAppliances(filterTerms) {
+    return filterTerms.every(filterTerm => this.isAppliance(filterTerm))
+  }
+
+  isMatchUstensils(filterTerms) {
+    return filterTerms.every(filterTerm => this.isUstensil(filterTerm))
+  }
+
+  isMatchRecipe(filter) {
+    const activeFilterCats = Object.keys(filter).filter(filterCat => filter[filterCat].length > 0)
+    if (activeFilterCats.length === 0) return true
+
+    const isMatch = ({ filterCat, filterTerms }) => {
+      if (filterCat === "main") return this.isMatchMain(filterTerms)
+      if (filterCat === "ingredients") return this.isMatchIngredients(filterTerms)
+      if (filterCat === "appliances") return this.isMatchAppliances(filterTerms)
+      if (filterCat === "ustensils") return this.isMatchAppliances(filterTerms)
+      return true
+    }
+
+    // if there is one fail test return false
+    return !activeFilterCats.some(filterCat => !isMatch({ filterCat, filterTerms: filter[filterCat] }))
+  }
 }
